c84ba3d5a0fa8425f997ebb2e8ed052d63a22d85
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-04-22 16:45:43 -0300

    Feat: Classe CONTA

--- Poo_Bank_System.py
+++ Poo_Bank_System.py
@@ -21,3 +21,62 @@ class PessoaFisica(Cliente):
         self.nome = nome
         self.data_nascimento = data_nascimento
         self.cpf = cpf
+        
+# Classe CONTA
+class Conta:
+    def __init__(self, numero, cliente):
+        self._saldo = 0
+        self._numero = numero
+        self._agencia = "0001"
+        self._cliente = cliente
+        self._historico = Historico()
+        
+    @classmethod
+    def nova_conta(cls, cliente, numero):
+        return cls(numero, cliente)
+    
+    @property
+    def saldo(self):
+        return self._saldo
+    
+    @property
+    def numero(self):
+        return self._numero
+    
+    @property
+    def agencia(self):
+        return self._agencia
+    
+    @property
+    def cliente(self):
+        return self._cliente
+    
+    @property
+    def historico(self):
+        return self._historico
+    
+    def sacar(self, valor):
+        saldo = self.saldo
+        excedeu_saldo = valor > saldo
+        
+        if excedeu_saldo:
+            print("\n@@@ Operacao falhou! Voce nao tem saldo suficiente. @@@")
+            
+        elif valor > 0:
+            self._saldo -= valor
+            print("\n=== Saque realizado com sucesso! ===")
+            return True
+        
+        else:
+            print("\n@@@ Operacao falhou! O valor informado é invalido. @@@")
+            
+        return False
+    
+    def depositar(self, valor):
+        if valor > 0:
+            self._saldo += valor
+            print("\n=== Deposito realizado com sucesso! ===")
+        else:
+            print("\n@@@ Operacao falhou! O valor informado é invalido. @@@")
+            
+        return True
