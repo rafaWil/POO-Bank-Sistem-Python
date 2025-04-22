736fd0f5ed9eef5339e1668ea35eef3be984a2e0
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-04-22 17:41:08 -0300

    Feat: Classe SAQUE => Transacao

--- Poo_Bank_System.py
+++ Poo_Bank_System.py
@@ -143,3 +143,19 @@ class Transacao(ABC):
     @abstractmethod
     def registrar(self, conta):
         pass
+
+# Classe SAQUE => Transacao
+class Saque(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+        
+    @property
+    def valor(self):
+        return self._valor
+    
+    def registrar(self, conta):
+        sucesso_transacao = conta.sacar(self.valor)
+        
+        if sucesso_transacao:
+            conta_historico.adicionar_transacao(self)
+            
