536319bf60222d8a7438b5887650024cebfafd4d
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-04-22 17:09:38 -0300

    Feat: Classe HISTORICO

--- Poo_Bank_System.py
+++ Poo_Bank_System.py
@@ -113,3 +113,22 @@ class ContaCorrente(Conta):
             C/C:\t\t{self.numero}
             Titular:\t{self.cliente.nome}
         """
+
+
+# Classe HISTORICO
+class Historico:
+    def __init__(self):
+        self._transacoes = []
+        
+    @property
+    def transacoes(self):
+        return self._transacoes
+    
+    def adicionar_transacao(self, transacao):
+        self._transacoes.append(
+            {
+                "tipo": transacao.__class__.__name__,
+                "valor": transacao.valor,
+                "data": datetime.now().strftime("%d-%m-%Y %H:%M:%s"),
+            }
+        )
