1f5e6f8fda52409a06ecbc0c3f3cd43c36d2e04f
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-04-22 19:23:13 -0300

    Feat: Funcao EXIBIR EXTRATO

--- Poo_Bank_System.py
+++ Poo_Bank_System.py
@@ -232,7 +232,30 @@ def sacar(clientes):
     cliente.realizar_transacao(conta, transacao)
         
 def exibir_extrato(clientes):
-    pass
+    cpf = input("Informe o CPF do cliente: ")
+    cliente = filtrar_cliente(cpf, clientes)
+    
+    if not cliente:
+        print("\n@@@ Cliente nao encontrado! @@@")
+        return
+
+    conta = recuperar_conta_cliente(cliente)
+    if not conta:
+        return
+    
+    print("\n================= EXTRATO =================")
+    transacoes = conta.historico.transacoes
+    
+    extrato = ""
+    if not transacoes:
+        extrato = "Nao foram realizadas movimentacoes."
+    else: 
+        for transacao in transacoes:
+            extrato += f"\n{transacoes['tipo']}:\n\tR${transacao['valor']:.2f}"
+            
+    print(extrato)
+    print(f"\nSaldo:\n\tR$ {conta.saldo:.2f}")
+    print("========================================")    
 
 def criar_cliente(clientes):
     cpf = input("Informe o CPF (somente número): ")
