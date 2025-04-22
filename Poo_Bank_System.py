5596f161f6bf27756b60e4b757aa8532990d91fc
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-04-22 16:20:30 -0300

    Feat: Classe de PESSOA FISICA

--- Poo_Bank_System.py
+++ Poo_Bank_System.py
@@ -13,3 +13,11 @@ class Cliente:
         
     def adicionar_conta(self, conta):
         self.contas.append(conta)
+        
+# Classe PESSOA FISICA => Cliente
+class PessoaFisica(Cliente):
+    def __init__(self, nome, data_nascimento, cpf, endereco):
+        super().__init__(endereco) 
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
