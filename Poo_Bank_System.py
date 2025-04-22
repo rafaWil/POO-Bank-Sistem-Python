45e9966e75847dd640bab3ff14c1b4cb6a075bda
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-04-22 17:43:50 -0300

    Feat: Classe DEPOSITO

--- Poo_Bank_System.py
+++ Poo_Bank_System.py
@@ -157,5 +157,19 @@ class Saque(Transacao):
         sucesso_transacao = conta.sacar(self.valor)
         
         if sucesso_transacao:
-            conta_historico.adicionar_transacao(self)
+            conta.historico.adicionar_transacao(self)
             
+# Classe DEPOSITO => Transacao
+class Deposito(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+        
+    @property
+    def valor(self):
+        return self._valor
+    
+    def registrar(self, conta):
+        sucesso_transacao = conta.depositar(self.valor)
+        
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
