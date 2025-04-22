bc02bf4eb39c918d712918c996a0c5126fc93ff3
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-04-22 15:38:00 -0300

    Feat: Classe CLIENTE

--- Poo_Bank_System.py
+++ Poo_Bank_System.py
@@ -1,3 +1,15 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
 # Bibliotecas de uso para tempo e modelos abstratos
+
+# classe CLIENTE
+class Cliente:
+    def __init__(self, endereco):
+        self.endereco = endereco
+        self.contas = []
+        
+    def realizar_transacao(self, conta, transacao):
+        transacao.registrar(conta)
+        
+    def adicionar_conta(self, conta):
+        self.contas.append(conta)
