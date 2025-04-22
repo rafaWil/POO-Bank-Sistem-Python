c837eaa1625619c624b8315bc3e50e30a227e3e0
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-04-22 18:54:39 -0300

    Feat: Funcoes CRIAR CLIENTE - CRIAR CONTA - SACAR

--- Poo_Bank_System.py
+++ Poo_Bank_System.py
@@ -196,3 +196,70 @@ def recuperar_conta_cliente(cliente):
         
     # FIXME: nao permite cliente escolher a conta
     return cliente.contas[0]
+
+def depositar(clientes):
+    cpf = input("Informe o CPF do cliente: ")
+    cliente = filtrar_cliente(cpf, clientes)
+    
+    if not cliente:
+        print("\n@@@ Cliente não encontrado! @@@")
+        return
+    
+    valor = float(input("Informe o valor do deposito: "))
+    transacao = Deposito(valor)
+    
+    conta = recuperar_conta_cliente(cliente)
+    if not conta:
+        return
+    
+    cliente.realizar_transacao(conta, transacao)
+    
+def sacar(clientes):
+    cpf = input("Informe o CPF do cliente: ")
+    cliente = filtrar_cliente(cpf, clientes)
+    
+    if not cliente:
+        print("\n@@@ Cliente nao encontrado! @@@")
+        return
+    
+    valor = float(input("Informe o valor do saque: "))
+    transacao = Saque(valor)
+    
+    conta = recuperar_conta_cliente(cliente)
+    if not conta:
+        return
+    
+    cliente.realizar_transacao(conta, transacao)
+        
+def exibir_extrato(clientes):
+    pass
+
+def criar_cliente(clientes):
+    cpf = input("Informe o CPF (somente número): ")
+    cliente = filtrar_cliente(cpf, clientes)
+    
+    if cliente:
+        print("\n@@@ Ja existe cliente com esse CPF! @@@")
+        return
+    
+    nome = input("Informe o nome completo: ")
+    data_nascimento = input("Informe a data de nascimento(dd-mm-aaaa): ")
+    endereco = input("Informe o endereco (logradouro, nro - bairro - cidade/sigla estado): ")
+    
+    cliente = PessoaFisica(nome=nome, data_nascimento=data_nascimento, cpf=cpf, endereco=endereco)
+    
+    clientes.append(cliente)
+    
+    print("\n=== Cliente criado com sucesso! ===")
+    
+def criar_conta(numero_conta, clientes, contas):
+    cpf = input("Informe o CPF do cliente: ")
+    cliente = filtrar_cliente(cpf, clientes)
+    
+    if not cliente:
+        print("\n@@@ Cliente nao encontrado, fluxo de criacao de conta encerrado! @@@")
+        return
+    
+    conta = ContaCorrente.nova_conta(cliente=cliente, numero=numero_conta)
+    contas.append(conta)
+    cliente.contas.append(conta)
